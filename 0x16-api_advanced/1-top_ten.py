b92788c574fbac103fdc9eeb0a4c43208dca4ba9
flourishcodes <kunle@adeniran.com>
2018-08-30 13:03:41 -0700

    Print top ten topics from a subreddit on REDDIT

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,14 +9,14 @@ import requests as re
 def top_ten(subreddit):
     """returns the top ten topics in subreddit"""
     if subreddit is None or not isinstance(subreddit, str):
-        return None
+        print('None')
     headers = {'User-Agent': 'me'}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     data = re.get(url, headers=headers).json()
     try:
         children = data.get('data').get('children')
-        for child in children[0:9]:
-            print(child.get('data').get('title'))
     except BaseException:
         print('None')
         return
+    for child in children[0:9]:
+        print(child.get('data').get('title'))
