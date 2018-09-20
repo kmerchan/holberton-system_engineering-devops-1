b7a8a2c31eb20cdd884992e426025c506548061c
flourishcodes <kunle@adeniran.com>
2018-09-19 21:54:03 -0700

    Puppet file to increase the ulimit of nginx

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+# Increase the Ulimit for files
+exec { 'increase the uLimit of nginx':
+     command => 'sed -i \'s/15/15000/g\' /etc/default/nginx',
+     path => ['/bin']
+}
+exec { 'restart nginx':
+     command => 'service nginx restart',
+     path => ['/usr/bin']
